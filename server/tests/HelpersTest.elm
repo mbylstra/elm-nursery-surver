b7e2462b63f85569fa0222877cd5686ca7156dab
Michael Bylstra <mbylstra@gmail.com>
2017-12-10 18:21:25 +1100

    test coverage for HelpersTest

--- server/tests/HelpersTest.elm
+++ server/tests/HelpersTest.elm
@@ -4,6 +4,7 @@ import Expect exposing (Expectation, equalSets)
 import Test exposing (..)
 import Helpers exposing (qualifiedNameToPath, groupByFirstTupleItem)
 import Dict
+import Result
 
 
 suite : Test
@@ -36,5 +37,74 @@ suite =
                         |> groupByFirstTupleItem
                         |> Expect.equal
                             (Dict.fromList [ ( "a", [ 1 ] ), ( "b", [ 2 ] ) ])
+            ]
+        , describe "anyTrue"
+            [ test "return true if at least on is true" <|
+                \_ ->
+                    [ False, True, False ]
+                        |> Helpers.anyTrue
+                        |> Expect.equal True
+            , test "return true if all true" <|
+                \_ ->
+                    [ True, True, True ]
+                        |> Helpers.anyTrue
+                        |> Expect.equal True
+            , test "return false if none true" <|
+                \_ ->
+                    [ False, False, False ]
+                        |> Helpers.anyTrue
+                        |> Expect.equal False
+            ]
+        , describe "allTrue"
+            [ test "return false if not all are true" <|
+                \_ ->
+                    [ True, True, False ]
+                        |> Helpers.allTrue
+                        |> Expect.equal False
+            , test "return true if all are true" <|
+                \_ ->
+                    [ True, True, True ]
+                        |> Helpers.allTrue
+                        |> Expect.equal True
+            , test "return false if none are true" <|
+                \_ ->
+                    [ False, False, False ]
+                        |> Helpers.allTrue
+                        |> Expect.equal False
+            ]
+        , describe "removeDuplicates"
+            [ test "removes duplicates and preserves order" <|
+                \_ ->
+                    [ 1, 2, 2, 2, 3, 2, 3 ]
+                        |> Helpers.removeDuplicates
+                        |> Expect.equal [ 1, 2, 3 ]
+            ]
+        , describe "unsafeDictGet"
+            [ test "removes duplicates and preserves order" <|
+                \_ ->
+                    Dict.fromList [ ( 1, "a" ), ( 2, "b" ) ]
+                        |> Helpers.unsafeDictGet "error" 1
+                        |> Expect.equal "a"
+
+            -- , test "crashes if key doesn't exist" <|
+            -- currently no way to test this
+            ]
+        , describe "unsafeAssumeSuccess"
+            [ test "unwraps an Ok value" <|
+                \_ ->
+                    Helpers.unsafeAssumeSuccess (Result.Ok "hello")
+                        |> Expect.equal "hello"
+
+            -- , test "crashes if key doesn't exist" <|
+            -- currently no way to test this
+            ]
+        , describe "unsafeListHead"
+            [ test "unwraps successful result" <|
+                \_ ->
+                    Helpers.unsafeListHead [ "a" ]
+                        |> Expect.equal "a"
+
+            -- , test "crashes if key doesn't exist" <|
+            -- currently no way to test this
             ]
         ]
